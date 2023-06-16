30c868188514960ad87204d84deeadc3edf114d0
ahmadhussein10 <itsahmadhussin19@gmail.com>
2023-06-16 06:10:09 -0700

    c variables

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Prints the alphabet.
  *
@@ -7,12 +6,13 @@
  */
 int main(void)
 {
- char x [26] ={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'}; 
- for(int i=0;i<52;i++)
- {
-         putchar(x[i]);
- }
- putchar('\n');
- return 0;
+int i;
+char x [26] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+for (i = 0 ; i < 52 ; i++)
+}
+putchar(x[i]);
+}
+putchar('\n');
+return (0);
 }
 ~                  
