571b96ec3f3539e9a2eb2bd271c5f29aac3d58f5
ahmadhussein10 <itsahmadhussin19@gmail.com>
2023-06-16 06:19:02 -0700

    c variables

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,10 +7,15 @@
 int main(void)
 {
 int i;
-char x [26] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-for (i = 0 ; i < 52 ; i++)
+char x[26] = "abcdefghijklmnopqrstuvwxyz"
+char a[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+for (i = 0 ; i < 26 ; i++)
 {
 putchar(x[i]);
+}
+for (i = 26 ; i < 52 ; i++)
+}
+putchar(a[i]);
 }
 putchar('\n');
 return (0);
