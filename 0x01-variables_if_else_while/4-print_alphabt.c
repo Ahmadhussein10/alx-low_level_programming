0a708ffcb04dd6e134b68a90f15aed912257b610
ahmadhussein10 <itsahmadhussin19@gmail.com>
2023-06-16 06:45:53 -0700

    c variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Prints the alphabet except q and e.
  *
@@ -7,15 +6,14 @@
  */
 int main(void)
 {
-char x = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}; 
- for(int i=0;i<26;i++)
- {
-	 if (x[i]!=q && x[i]!= e)
-	 {
-		 putchar(i);
-	 }
- }
- putchar('\n');
- return 0;
+char x =  "abcdefghijklmnopqrstuvwxyz";
+for(int i = 0 ; i < 26 ; i++)
+{
+if (x[i] != q && x[i] != e)
+{
+putchar(i);
 }
-~                  
+}
+putchar('\n');
+return 0;
+}                  
