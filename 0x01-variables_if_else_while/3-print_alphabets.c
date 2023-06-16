ecf9024e344a175e9f690350c92835e48e46e3f1
ahmadhussein10 <itsahmadhussin19@gmail.com>
2023-06-16 06:21:29 -0700

    c variables

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,14 +7,14 @@
 int main(void)
 {
 int i;
-char x[26] = "abcdefghijklmnopqrstuvwxyz"
+char x[26] = "abcdefghijklmnopqrstuvwxyz";
 char a[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 for (i = 0 ; i < 26 ; i++)
 {
 putchar(x[i]);
 }
 for (i = 26 ; i < 52 ; i++)
-}
+{
 putchar(a[i]);
 }
 putchar('\n');
