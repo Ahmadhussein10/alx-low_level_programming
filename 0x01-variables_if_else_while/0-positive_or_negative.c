f6e3e472cfde5be9a7cc5f6dc6bec7702e5bfaba
ahmadhussein10 <itsahmadhussein19@gmail.com>
2023-06-16 07:33:21 -0700

    c variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,11 +18,11 @@ printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%d is zero", n);
+printf("%d is zero\n", n);
 }
 else
 {
-printf("%d negative", n);
+printf("%d negative\n", n);
 }
 return (0);
 }
