e531d7a89f7639b700c5d0820d349b18e38fa8df
ahmadhussein10 <itsahmadhussein19@gmail.com>
2023-06-16 07:02:58 -0700

    c variables

--- 0x01-variables_if_else_while/6-print_nunberz.c
+++ 0x01-variables_if_else_while/6-print_nunberz.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Prints numbers from 0 to 9.
  *
@@ -7,13 +6,11 @@
  */
 int main(void)
 {
-	int i;
-
-	for (i = 48; i < 58; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
-	return (0);
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
 }
-
+putchar('\n');
+return (0);
+}
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,15 +7,15 @@
  */
 int main()
 {
-	int i;
+int i;
 
-	for (i = 48; i < 58; i++)
-	{
-		putchar(i);
-	}
-	for (i = 65; i < 71; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
-	return 0;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+for (i = 65; i < 71; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return(0);
