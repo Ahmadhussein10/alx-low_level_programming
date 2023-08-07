27d703fcff6171f9c219f371e564e8884321dc32
ahmadhussein10 <itsahmdhussein19@gmail.com>
2023-08-07 02:07:15 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t r;
 
 	f = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (f == -1)
 		return (0);
 	bu = malloc(sizeof(char) * letters);
 	r = read(f, bu, letters);
